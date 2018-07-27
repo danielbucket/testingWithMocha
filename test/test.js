584f1138da86634a0937222c2b43b6bf1055926c
Daniel Bucket <danieljbucket@gmail.com>
2018-07-27 10:05:25 -0600

    update

--- test/test.js
+++ test/test.js
@@ -4,16 +4,14 @@ const testOne = require('../testOne/testone.js');
 import testTwo from '../testTwo/testTwo';
 // using the import keyword with Mocha you must first transpile
 
-describe('Array', () => {
-	describe('testOne', () => {
-		it('01: should add numbers', () => {
-			assert.equal(testOne(-275,279), 4)
-		})
-	});
+describe('testOne', function() {
+	it('01: should add numbers', function() {
+		assert.equal(testOne(-275,279), 4)
+	})
+});
 
-	describe('testTwo', () => {
-		it('01: should multiply numbers', () => {
-			assert.equal(testTwo(5,5), 25)
-		})
-	});
+describe('testTwo', () => {
+	it('01: should multiply numbers', () => {
+		assert.equal(testTwo(5,5), 25)
+	})
 });
