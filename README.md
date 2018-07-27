a21322dd3d5de07ad4f06e2e587ed2e3911be999
Daniel Bucket <danieljbucket@gmail.com>
2018-07-27 10:01:45 -0600

    update testTwo

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ***This is a tutorial for setting up basic testing using Mocha and incorporating Babel and webPack. There are several branches each corresponding to a particular aspect of the testing process.***
 
 ***testTwo:***
-testTwo utilizes the import and export.
+testTwo utilizes the import and export statements found in ES and beyond.
 http://jamesknelson.com/testing-in-es6-with-mocha-and-babel-6/
--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
 const assert = require('assert');
+const testOne = require('../testOne/testone.js');
 
-import testOne from '../testOne/testOne';
 import testTwo from '../testTwo/testTwo';
 // using the import keyword with Mocha you must first transpile
 
 describe('Array', () => {
 	describe('testOne', () => {
 		it('01: should add numbers', () => {
-			assert.equal(testOne(1,3), 4)
+			assert.equal(testOne(-275,279), 4)
 		})
 	});
 
 	describe('testTwo', () => {
 		it('01: should multiply numbers', () => {
-			assert.equal(testTwo(2,3), 6)
+			assert.equal(testTwo(5,5), 25)
 		})
 	});
 });
