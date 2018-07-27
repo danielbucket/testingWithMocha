feaff025b7425ce9926467811a7ceb8c65aa25be
Daniel Bucket <danieljbucket@gmail.com>
2018-07-27 10:17:01 -0600

    update

--- test/test.js
+++ test/test.js
@@ -2,16 +2,17 @@ const assert = require('assert');
 const moduleOne = require('../moduleOne/moduleOne.js');
 
 import moduleTwo from '../moduleTwo/moduleTwo.js';
-// using the import keyword with Mocha you must first transpile
+// to use the import statements with Mocha you must first
+// transpile the code using Babel
 
 describe('moduleOne', function() {
 	it('01: should add numbers', function() {
 		assert.equal(moduleOne(-275,279), 4)
-	})
+	});
 });
 
 describe('moduleTwo', () => {
 	it('01: should multiply numbers', () => {
 		assert.equal(moduleTwo(5,5), 25)
-	})
+	});
 });
